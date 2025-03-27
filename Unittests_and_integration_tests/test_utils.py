1e3916a12e35f30e1522bd5509c6fa560e268e2d
LD <lionelmonthe184@gmail.com>
2025-03-27 12:52:28 +0100

    Parameterize and patch

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ import unittest
 from utils import access_nested_map
 from utils import get_json
 from unittest.mock import Mock, patch
+from utils import memoize
 from parameterized import parameterized
 
 
@@ -36,12 +37,12 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
     """ Test the function get_json """
     @parameterized.expand([
-        ("http://example.comm", {"payload": True}),
+        ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
         """ Test that get_json returns the correct payload and calls requests.get once """
-                # patch remplace 'requests.get' par un mock
+        # patch remplace 'requests.get' par un mock
         with patch("utils.requests.get") as mock_get:
             # On prépare un mock qui retournera un .json() simulé
             mock_response = Mock()
@@ -52,3 +53,26 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test the function memoize """
+    def test_memoize(self):
+        """ Test that when calling a_property twice, the correct result is returned but a_method is only called once using
+        assert_called_once """
+        class TestClass:
+            def a_method(self):
+                return 42
+            
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        # patch remplace '' par un mock
+        with patch.object(TestClass, "a_method", return_value=42) as mocked_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mocked_method.assert_called_once()
